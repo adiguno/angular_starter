d0384c51db6b5dda30265dd17152fe91d3486a48
Guo, Dian <dianguo@gatech.edu>
2019-11-27 18:01:13 -0500

    added 2 way data binding: textbox and hero.name

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+// import the FormsModule from the forms library, whre NgModel lives
+import {FormsModule} from '@angular/forms'; 
+
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
 
+// annotates the top-level AppModule class
 @NgModule({
   declarations: [
     AppComponent,
     HeroesComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule // import the FormsModule external module for the App
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,11 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 
+// imoprt the custom Hero class src/app/hero.ts
+import {Hero} from '../hero';
+
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
+
+  // initialize the Hero object
+  hero: Hero = {
+    id: 1,
+    name: 'Windstorm'
+  };
 
   constructor() { }
 
