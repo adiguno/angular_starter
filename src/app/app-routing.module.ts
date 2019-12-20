d8eb673e60f437b3b301a8a9354d81ba925f9ea9
Guo, Dian <dianguo@gatech.edu>
2019-12-20 14:43:14 -0500

    addd routing to 'dashboard' component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes} from '@angular/router';
 import { HeroesComponent } from './heroes/heroes.component';
+import { DashboardComponent }   from './dashboard/dashboard.component';
 
 // loads the HeroesComponent when navigating to its URL
+// URL: localhost:4200/heroes
+// URL: localhost:4200/dashboard
 const routes: Routes = [
-  {
-    path: 'heroes', // URL: localhost:4200/heroes
-    component: HeroesComponent
-  }
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full' }, // redirect default route to dashboard
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'heroes',component: HeroesComponent}
 ];
 
 // gives the Router somewhere to go after the routes have been configured
