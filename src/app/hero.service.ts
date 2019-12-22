7c789ec887649f1e428c38f0e214c5b8e542ee4c
Guo, Dian <dianguo@gatech.edu>
2019-12-21 20:16:47 -0500

    error handling dep, retrieves data from URL

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,12 +1,14 @@
 import { Injectable } from '@angular/core';
 import { Observable, of, ObjectUnsubscribedError } from 'rxjs';
 
-import {Hero} from './hero';
+import { Hero } from './hero';
 import { HEROES } from './mock-heroes';
-import {MessageService} from './message.service';
+import { MessageService } from './message.service';
 
 // HTTP
 import { HttpClient, HttpHeaders } from '@angular/common/http';
+// error handling
+import { catchError, map, tap } from 'rxjs/operators';
 
 
 // marks the class that it participates in 
@@ -30,11 +32,17 @@ export class HeroService {
   // get the mock heroes
   getHeroes(): Observable<Hero[]> {
     // add a message to the service
-    this.messageService.add('HeroService: feteched heroes');
+    // this.messageService.add('HeroService: feteched heroes');
 
     // emits a single value, the array of mock heroes
-    return of(HEROES); // returns an Observable<Hero[]>
+    // return of(HEROES); // returns an Observable<Hero[]>
 
+    // use the mock server's data, via URL
+    return this.http.get<Hero[]>(this.heroesUrl)
+    .pipe(
+      tap(_ => this.log('fetched heroes')),
+      catchError(this.handleError<Hero[]>('getHeroes', []))
+    );
   }
 
   // get hero based on id
@@ -47,5 +55,28 @@ export class HeroService {
   private log(message: string) {
     this.messageService.add(`HeroService: ${message}`);
   }
+
+  // handling errors
+  // generalized to handle many 'HeroService' methods
+  /**
+   * Handle Http operation that failed.
+   * Let the app continue.
+   * @param operation - name of the operation that failed
+   * @param result - optional value to return as the observable result
+   */
+  private handleError<T> (operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+
+      // TODO: send the error to remote logging infrastructure
+      console.error(error); // log to console instead
+
+      // TODO: better job of transforming error for user consumption
+      this.log(`${operation} failed: ${error.message}`);
+
+      // Let the app keep running by returning an empty result.
+      return of(result as T);
+    };
+  }
+
 }
 
